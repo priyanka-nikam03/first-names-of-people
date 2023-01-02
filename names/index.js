abb6f03c4ae2a79f076e89136f723449084073d0
priyanka <priyanka.nikam03@yahoo.in>
2023-01-02 20:03:07 +0000

    get first name

---
+++ names/index.js
@@ -0,0 +1,6 @@
+const peopleNames = require("../country/state/city/index.js");
+const getFirstNames = require("../utilities/utils/index.js");
+const getPeopleInCity = (peopleNames) => {
+  return getFirstNames(peopleNames);
+};
+module.exports = getPeopleInCity;
